16f032aaecb6828bef8d0af7a7028e437ed6a4ac
admin <D.Alavaro@gmail.com>
2022-10-19 07:44:56 +0300

    18/10/2022 h

--- part1/simple_print/main.py
+++ part1/simple_print/main.py
@@ -10,8 +10,19 @@
 # TODO Вы можете попробовать задекорировать функцию func
 # в теле которой ничего не происходит.
 
+from flask import request, abort, Flask
+from flask_restx import Api, Resource
+
+def called(x):
+    def wrapper():
+        print('функция вызвана')
+
+    return wrapper
+
+@called
 def func():
     pass
+
 
 if __name__=="__main__":
     func()
--- part1/simple_wrapper/main.py
+++ part1/simple_wrapper/main.py
@@ -3,6 +3,9 @@
 # результат выполнения функции превращать 
 # в json строку и возвращать эту строку. 
 # Примем на веру, что данные всегда или словарь или список.
+import json
+
+from flask import jsonify
 
 
 # TODO напишите декратор jsonwrap здесь
@@ -11,6 +14,12 @@
 # Ниже следует код для самопроверки:
 # TODO Вы можете попробовать задекорировать функцию func
 # которая возвращает словарь.
+
+def jsonwrap(x):
+    def wrapper():
+        r = x()
+        return json.dumps(r)
+    return wrapper
 
 def func():
     return {'text': 'tasks',
